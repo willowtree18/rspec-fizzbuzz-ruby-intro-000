44556799c1f323b489806ef8b9bfd10a8664e844
Emily Willow <emily.shapiera@gmail.com>
2020-11-22 17:23:15 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
-if fizz_3 = 3
+if fizz_3 == 3
   return ("Fizz")
 
-  elsif fizz_5 = 5
+elsif fizz_5 == 5
     return ("Buzz")
 
-  elsif fizz_15 = 15
+  elsif fizz_15 == 15
     return ("Fizzbuzz")
 
-  elsif fizzbuzz_4 = 4
+  elsif fizzbuzz_4 == 4
     return nil
 
   end
