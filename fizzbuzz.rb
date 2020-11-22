1f530116bf15260e1ee9afa48e6e09cf15af4605
Emily Willow <emily.shapiera@gmail.com>
2020-11-22 17:14:38 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(fizz_3)
-return ("Fizz")
+puts ("Fizz")
 end
 
 def fizzbuzz(fizz_5)
-  return ("Buzz")
+  puts ("Buzz")
 end
