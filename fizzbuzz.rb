979e43775ccab3fc173916a8f838a5ad1b1570e2
Emily Willow <emily.shapiera@gmail.com>
2020-11-22 17:12:03 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(fizz_3)
+  it 'returns "Fizz" when the number is divisible by 3' do
 expect(fizz_3)to.eq("Fizz")
 end
