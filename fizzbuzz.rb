3fe64e522351b7ffdb1054d60b9e287eecce66b6
Emily Willow <emily.shapiera@gmail.com>
2020-11-22 17:33:44 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,16 +6,16 @@ fizz_15 = 15
 fizz_4 = 4
 
 def fizzbuzz(variable)
-if variable == 3
+if fizz_3 == 3
   return ("Fizz")
 
-elsif variable == 5
+elsif fizz_5 == 5
     return ("Buzz")
 
-  elsif variable == 15
+  elsif fizz_15 == 15
     return ("Fizzbuzz")
 
-  elsif variable == 4
+  elsif fizz_4 == 4
     return nil
 
   end
